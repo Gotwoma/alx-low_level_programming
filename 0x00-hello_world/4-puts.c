ced968f5ac3a26522041a8c398e30c1be4a6cc81
Grace Ateka <graceotwoma@gmail.com>
2023-04-20 20:16:39 +0300

    Hello.puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main function is the entry point of the program
+ * functions should indicate return type
+ * return 0 indicates success
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
