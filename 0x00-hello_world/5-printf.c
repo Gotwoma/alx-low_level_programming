3d91b699f7abb2db0a73784ce8963a450d2a345d
Grace Ateka <graceotwoma@gmail.com>
2023-04-20 19:47:43 +0300

    Hello,puts

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main function - entry point of the program
+ *function should have a return type
+ * return is zero
+ * puts writes string and a trailing newline to standard out
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return 0;
+}
