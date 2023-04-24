201aa7b9fb30b43cc61e9310fa591368d4b04348
Grace Ateka <graceotwoma@gmail.com>
2023-04-24 21:34:05 +0300

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 to indicate success
+ */
+int main(void)
+{
+	char ch;
+	char CH;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (CH = 'A'; CH <= 'Z'; CH++)
+	{
+		putchar(CH);
+	}
+	putchar('\n');
+	return (0);
+}
