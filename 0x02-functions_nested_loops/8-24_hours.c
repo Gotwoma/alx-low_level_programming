20de22ec7586e19ad6ebfc4a5d58896e715c119d
Grace Ateka <graceotwoma@gmail.com>
2023-04-25 17:16:23 +0300

    8-24_hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,15 +16,16 @@ void jack_bauer(void)
 			{
 				for (d = 0; d <= 9; d++)
 				{
-					if (a >= 2 && b >= 4)
-						break;
 					_putchar(a + 48);
 					_putchar(b + 48);
 					_putchar(58);
 					_putchar(c + 48);
 					_putchar(d + 48);
 					_putchar('\n');
-					return;
+					if (a >= 2 && b >= 4)
+					{
+						return;
+					}
 				}
 			}
 		}
