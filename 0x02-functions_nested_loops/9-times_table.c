612fee8412aa2026587bbdfb70e5cc08a7e7bc8b
Grace Ateka <graceotwoma@gmail.com>
2023-04-25 15:50:24 +0300

    9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * times_table - print 9 times table starting with 0
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int a, b;
+
+	for (b = 1; b < 10; b++)
+	{
+		_putchar('0');
+		for (a = 1; a < 10; a++)
+		{
+			int m = a * b;
+
+			_putchar(',');
+			if (m / 100 == 0)
+			{
+				_putchar(32);
+			}
+			if (m / 10 == 0)
+			{
+				_putchar(32);
+			}
+			else
+			{
+				_putchar('0' + (m / 10));
+			}
+			if (m % 10 == 0)
+			{
+				 _putchar('0');
+			}
+			else
+			{
+				_putchar('0' + (m % 10));
+			}
+		}
+		_putchar('\n');
+	}
+}
+
