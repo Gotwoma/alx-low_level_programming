cfac1e76b45547b8f27bf9b4320c2cd465d090d7
Grace Ateka <graceotwoma@gmail.com>
2023-04-27 16:28:47 +0300

    3-print_numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints the numbers 0 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
