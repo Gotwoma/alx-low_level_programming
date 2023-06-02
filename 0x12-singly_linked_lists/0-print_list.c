5f8275d21912d7ea39da11e1f5dd12674efa0219
Grace Ateka <graceotwoma@gmail.com>
2023-06-02 23:14:57 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,14 +12,14 @@ size_t print_list(const list_t *h)
 {
 	size_t p_l = 0;
 
-	while (h)
-	{
+	do {
 		if (!h->str)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		p_l++;
-	}
+	}  while (h);
+
 	return (p_l);
 }
